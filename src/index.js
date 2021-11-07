f06eb68cd7a24a5cb4151035c08bd4eefd38ea59
jpbluesman <jor.perezo@hotmail.com>
2021-11-06 21:13:27 -0300

    First commit
    Todos app with redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+import { asyncMiddleware } from './middlewares/async';
 import reportWebVitals from './reportWebVitals';
+import { reducer } from './features/todos';
+
+const store = createStore(reducer, applyMiddleware(asyncMiddleware))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
